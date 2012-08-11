49941187399418de885b9698d9aa046b0fedd1ce
beckmi <beckmi@c17157d5-08f2-41e7-a131-df055f6a0da3>
2012-08-11 12:46:10 +0000

    Removing spaces from "define", which is required for Transifex per DCrusader

--- XoopsModules/uitabs/releases/1.01/htdocs/modules/uitabs/language/english/main.php
+++ XoopsModules/uitabs/releases/1.01/htdocs/modules/uitabs/language/english/main.php
@@ -1,7 +1,7 @@
 <?php
 	
 	// smarty constants
-	define('_MN_TABS_SUBSCRIBE', 'Subsciber to Video');
-	define('_MN_TABS_RATE', 'Rate Video');
+	define('_MN_TABS_SUBSCRIBE','Subsciber to Video');
+	define('_MN_TABS_RATE','Rate Video');
 	
 ?>
