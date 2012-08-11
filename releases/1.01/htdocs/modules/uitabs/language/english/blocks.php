2d63c28bd174081418619c64e78ad11fab4a2348
beckmi <beckmi@c17157d5-08f2-41e7-a131-df055f6a0da3>
2012-08-11 12:46:10 +0000

    Removing spaces from "define", which is required for Transifex per DCrusader

--- releases/1.01/htdocs/modules/uitabs/language/english/blocks.php
+++ releases/1.01/htdocs/modules/uitabs/language/english/blocks.php
@@ -1,8 +1,8 @@
 <?php
 
 	// Form Titles
-	define('_BL_UITABS_TABS_ITEMS', 'Items per tab:&nbsp;');
-	define('_BL_UITABS_FLOWPLAYER_BLOCK_WIDTH', 'Video Player Width:&nbsp;');
-	define('_BL_UITABS_FLOWPLAYER_BLOCK_HEIGHT', 'Video Player Height:&nbsp;');
+	define('_BL_UITABS_TABS_ITEMS','Items per tab:&nbsp;');
+	define('_BL_UITABS_FLOWPLAYER_BLOCK_WIDTH','Video Player Width:&nbsp;');
+	define('_BL_UITABS_FLOWPLAYER_BLOCK_HEIGHT','Video Player Height:&nbsp;');
 	
 ?>
